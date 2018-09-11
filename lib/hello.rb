6aa6712e33e403ea5b45cb35f7ccf895ea80779d
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-11 02:00:28 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,6 @@
 def hello_t(array)
   
-  if block_given? == true
-    puts "given=================================================="
-  end
-  
+  block_given? == true puts "given====" : puts "not given!!!!!!!!!!!!!!!!!!"
 #block_given == true? (array.each{|name| puts name}) : (puts "Hey! No block was given!")
 array
 end
