df2fb1276c947cdb983aae90857f77bbadfab985
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-11 02:08:56 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,14 @@
 def hello_t(array)
 #block_given? ? (array.each{|name| puts name}) : (puts "Hey! No block was given!")
 
-if block_given?
-  i = 0
-  
-  while i < array.length
-    yield(array[i])
-    i += 1
+  if block_given?
+    i = 0
+    
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
   end
-end
 array
 end
 
